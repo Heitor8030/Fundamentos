803157978f48ade71dd4402aea1a0e592b5f379a
Heitor <Heitorccfernandes550@gmail.com>
2024-05-17 22:20:19 -0300

    Add files via upload

---
+++ Python/Estruturas condicionais/if-else.py
@@ -0,0 +1,12 @@
+# Definindo a variável que será avaliada
+temperatura = 25
+
+# Estrutura
+if temperatura < 0:
+    print("Está muito frio!")
+elif temperatura < 15:
+    print("Está frio.")
+elif temperatura < 25:
+    print("Está agradável.")
+else:
+    print("Está quente.")
---
+++ Python/Estruturas de repetição/continue.py
@@ -0,0 +1,4 @@
+for cont in range(1, 6):
+    if cont == 3:
+        continue
+    print(cont)
