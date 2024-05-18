b44aeb47b8d158b938108b7bc530e29fe97082de
Heitor <Heitorccfernandes550@gmail.com>
2024-05-17 22:19:57 -0300

    Delete Python/Estruturas de repetição directory

--- Python/Estruturas de repetição/for.py
+++
@@ -1,5 +0,0 @@
-# Exemplo de loop for para imprimir os números de 1 a 5
-for cont in range(1, 6):
-    # O loop irá iterar de 1 a 5 (não incluindo o 6)
-    print(cont)
-    # Imprime o valor atual da variável
